278e5e097bfbe229f68a877c4066a71ecf04023e
Ubuntu <ubuntu@ip-172-31-17-96.us-west-2.compute.internal>
2013-08-19 05:59:29 +0000

    fix receive dice values

--- gameroom.erl
+++ gameroom.erl
@@ -5,6 +5,7 @@
 start(Player1_uid,Player2_uid) ->
     io:format("start. ~n",[]),
     Pid = spawn(gameroom, init_gameroom, [Player1_uid,Player2_uid]),
+    %need monitor the game room!
     io:format("start: pid: ~w~n",[Pid]),
     %% register(gameroom_proc, Pid),
     io:format("start. ~n",[]),
@@ -15,18 +16,30 @@ init_gameroom(Player1_uid,Player2_uid) ->
         {join, Player2_uid} ->
         io:format("join. ~s~n",[Player2_uid]),
         wait_for_p1_rolldice();
-        {_, _} -> 
-        io:format("command not found. ~n",[])
+        {_ , Player2_uid} -> 
+            io:format("player 2 can only join. ~n",[]),
+            {error,'player 2 can only join.'}
     end.
 
 wait_for_p1_rolldice() ->
     receive
-        {p1_roll,Player1_uid} ->
+        {p1_roll,Player1_uid,FromPid} ->
+        io:format("p1 roll dice ~n",[]),
         Dice1_value = random:uniform(6),
         Dice2_value = random:uniform(6),
         Dice3_value = random:uniform(6),
-        io:format("p1 roll dice: ~w,~w,~w~n",[Dice1_value,Dice2_value,Dice2_value]),
-        [Dice1_value,Dice2_value,Dice2_value]
+        Dice4_value = random:uniform(6),
+        Dice5_value = random:uniform(6),
+        io:format("p1 roll dice: ~w,~w,~w,~w,~w ~n",[Dice1_value,Dice2_value,Dice3_value,Dice4_value,Dice5_value]),
+        FromPid ! {dice_result,Dice1_value,Dice2_value,Dice3_value,Dice4_value,Dice5_value},
+        %wait_for_p1_makecall()
+        wait_for_p1_rolldice()
+    end.
+
+wait_for_p1_makecall() ->
+    receive
+        {p1_bet,Player1_uid,PlayerBetAmound,FromPid} ->
+            true
     end.
 
 join_gameroom(Pid,Player2_uid) ->
@@ -34,7 +47,7 @@ join_gameroom(Pid,Player2_uid) ->
 
 player1_rolldice(Pid,Player1_uid) -> 
     io:format("roll dice. ~n",[]),
-    Pid ! {p1_roll,Player1_uid}.
+    Pid ! {p1_roll,Player1_uid,self()}.
 
 out(Arg) ->
     io:format("main. ~n",[]),
@@ -51,12 +64,21 @@ out(Arg, ["init", "p1_uid", Player1_uid, "p2_uid", Player2_uid]) ->
     StringConverted = lists:map(ConvertFun, Response),
     Output = mochijson2:encode({struct, StringConverted}),
     {html, Output};
+
 out(Arg, ["join", "room_pid", Pid, "uid", Player2_uid]) -> 
     io:format("join. ~w~n",[list_to_pid(Pid)]),
     join_gameroom(list_to_pid(Pid), Player2_uid),
     {html, Player2_uid};
-out(Arg, ["rolldice", "room_pid", Pid, "p1_uid", Player1_uid]) -> 
-    io:format("rolldice. ~n",[]),
-    player1_rolldice(Pid,Player1_uid).
 
+out(Arg, ["rolldice", "room_pid", Pid, "p1_uid", Player1_uid]) -> 
+    io:format("!rolldice. ~n",[]),
+    player1_rolldice(list_to_pid(Pid),Player1_uid),
+    receive
+        {dice_result,Dice1_value,Dice2_value,Dice3_value,Dice4_value,Dice5_value} ->
+        %build dice result json.
+    io:format("receive dice result. ~n",[]),
+        DiceResultJson = {dice_result,[Dice1_value,Dice2_value,Dice3_value,Dice4_value,Dice5_value]},
+        DiceResultJsonStr = mochijson2:encode({struct, [DiceResultJson]}),
+        {html, DiceResultJsonStr}
+    end.
 
